1853db206b9d63363eb6d9459d8cbde77810c3b9
Zulfurix <Zulfurix@gmail.com>
2020-05-12 14:46:13 +0100

    Add Newtonsoft.Json

---
+++ FivePD-GangWarfare/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace FivePD_GangWarfare
+{
+    class Config : BaseScript
+    {
+        string configFile = LoadResourceFile("fivepd", "callouts/FivePD-GangWarfare.json");
+    }
+}
